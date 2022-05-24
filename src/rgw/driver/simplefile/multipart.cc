0925e7171471629d91b4c9df4a77400d9c8933ae
Joao Eduardo Luis <joao@suse.com>
2022-05-24 08:12:49 +0000

    rgw/sfs: add basic write support
      note: contains transient code.
    
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

---
+++ src/rgw/driver/simplefile/multipart.cc
@@ -0,0 +1,185 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t
+// vim: ts=8 sw=2 smarttab ft=cpp
+/*
+ * Ceph - scalable distributed file system
+ * Simple filesystem SAL implementation
+ *
+ * Copyright (C) 2022 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ */
+#include "rgw_sal_simplefile.h"
+#include "multipart.h"
+#include "writer.h"
+
+#define dout_subsys ceph_subsys_rgw
+
+using namespace std;
+
+
+namespace rgw::sal {
+
+SimpleFileMultipartObject::SimpleFileMultipartObject(
+  CephContext *cct,
+  const std::string &_oid,
+  const std::string &_upload_id
+) {
+  ldout(cct, 10) << " has upload_id: " << _upload_id << dendl;
+  init(cct, _oid, _upload_id);
+}
+
+SimpleFileMultipartObject::SimpleFileMultipartObject(
+  CephContext *cct,
+  const std::string &_oid,
+  const std::optional<std::string> _upload_id
+) {
+  std::string upid = (_upload_id.has_value() ? _upload_id.value() : "");
+  init(cct, _oid, upid);
+}
+
+std::string SimpleFileMultipartObject::gen_upload_id() {
+  auto now = ceph::real_clock::now();
+  return ceph::to_iso_8601_no_separators(now, ceph::iso_8601_format::YMDhms); 
+}
+
+void SimpleFileMultipartObject::init(
+  CephContext *_cct,
+  std::string _oid,
+  std::string _upload_id
+) {
+  boost::trim(_upload_id);
+  if (_upload_id.empty()) {
+    _upload_id = gen_upload_id();
+  }
+  ldout(_cct, 10) << "multipart_object::init: upload_id: [" << _upload_id
+                 << "]" << dendl;
+  oid = _oid;
+  upload_id = _upload_id;
+  meta = "_meta." + oid + "." + upload_id;
+}
+
+std::unique_ptr<rgw::sal::Object> SimpleFileMultipartUpload::get_meta_obj() {
+  return bucket->get_object(
+    rgw_obj_key(get_meta(), string(), RGW_OBJ_NS_MULTIPART)
+  );
+}
+
+int SimpleFileMultipartUpload::write_metadata(
+  const DoutPrefixProvider *dpp,
+  SimpleFileMultipartMeta &metadata
+) {
+
+  auto obj = get_meta_obj();
+
+  bufferlist bl;
+  encode(metadata, bl);
+
+  SimpleFileBucket *b = static_cast<SimpleFileBucket*>(bucket);
+  std::filesystem::path metafn = b->objects_path() / get_meta();
+  bl.write_file(metafn.c_str());
+
+  lsfs_dout(dpp, 10) << "wrote metadata to " << metafn
+                     << ", len: " << bl.length() << dendl;
+  return 0;
+}
+
+int SimpleFileMultipartUpload::init(
+  const DoutPrefixProvider *dpp,
+  optional_yield y,
+  ACLOwner &owner,
+  rgw_placement_rule &dest_placement,
+  rgw::sal::Attrs &attrs
+) {
+
+  lsfs_dout(dpp, 10) << "owner: " << owner.get_display_name()
+                     << ", attrs: " << attrs << dendl;
+  lsfs_dout(dpp, 10) << "objid: " << get_key() << ", upload_id: "
+                     << get_upload_id() << ", meta: " << get_meta() << dendl;
+
+  SimpleFileMultipartMeta metadata(
+    owner, attrs, dest_placement, RGWObjCategory::MultiMeta,
+    PUT_OBJ_CREATE_EXCL,  // this comes from rgw_rados.h, we should change it.
+    mtime
+  );
+  write_metadata(dpp, metadata);
+
+  lsfs_dout(dpp, 10) << "return" << dendl;
+  return 0;
+}
+
+int SimpleFileMultipartUpload::list_parts(
+  const DoutPrefixProvider *dpp,
+  CephContext *cct,
+  int num_parts,
+  int marker,
+  int *next_marker,
+  bool *truncated,
+  bool assume_unsorted
+) {
+  lsfs_dout(dpp, 10) << "return" << dendl;
+  return 0;
+}
+
+int SimpleFileMultipartUpload::abort(
+  const DoutPrefixProvider *dpp,
+  CephContext *cct
+) {
+  lsfs_dout(dpp, 10) << "return" << dendl;
+  return 0;
+}
+
+int SimpleFileMultipartUpload::complete(
+  const DoutPrefixProvider *dpp,
+  optional_yield y,
+  CephContext *cct,
+  std::map<int, std::string> &part_etags,
+  std::list<rgw_obj_index_key> &remove_objs,
+  uint64_t &accounted_size,
+  bool &compressed,
+  RGWCompressionInfo &cs_info,
+  off_t &ofs,
+  std::string &tag,
+  ACLOwner &owner,
+  uint64_t olh_epoch,
+  rgw::sal::Object *target_obj
+) {
+  lsfs_dout(dpp, 10) << "return" << dendl;
+  return 0;
+}
+
+int SimpleFileMultipartUpload::get_info(
+  const DoutPrefixProvider *dpp,
+  optional_yield y,
+  rgw_placement_rule **rule,
+  rgw::sal::Attrs *attrs
+) {
+  lsfs_dout(dpp, 10) << "return" << dendl;
+  return 0;
+}
+
+std::unique_ptr<Writer> SimpleFileMultipartUpload::get_writer(
+  const DoutPrefixProvider *dpp,
+  optional_yield y,
+  std::unique_ptr<rgw::sal::Object> head_obj,
+  const rgw_user &_owner,
+  const rgw_placement_rule *ptail_placement_rule,
+  uint64_t part_num,
+  const std::string &part_num_str
+) {
+  lsfs_dout(dpp, 10) << "head obj: " << head_obj << ", owner: " << _owner
+                     << ", part num: " << part_num << dendl;
+
+  return std::make_unique<SimpleFileMultipartWriter>(
+    dpp, y, this, std::move(head_obj), store, _owner,
+    ptail_placement_rule, part_num, part_num_str
+  );
+}
+
+void SimpleFileMultipartUpload::dump(Formatter *f) const {
+  // TODO
+}
+
+} // ns rgw::sal
