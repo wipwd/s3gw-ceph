a40ea10a4a9fed5c25d82cf1183f1b0e0cdfa49d
Joao Eduardo Luis <joao@suse.com>
2022-06-01 06:10:55 +0000

    rgw/sfs: keep objects hashed on disk
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

--- src/rgw/driver/simplefile/object.h
+++ src/rgw/driver/simplefile/object.h
@@ -230,6 +230,9 @@ class SimpleFileObject : public StoreObject {
     return 0;
   }
 
+  std::filesystem::path get_data_path();
+  std::filesystem::path get_metadata_path();
+
   const std::string get_cls_name() { return "object"; }
   void write_meta();
   void load_meta();
