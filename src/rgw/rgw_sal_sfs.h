de2d2a7f060c42b4aa2374f294c0c9a10b8275f2
Giuseppe Baccini <giuseppe.baccini@suse.com>
2022-08-22 14:01:59 +0000

    rgw/sfs: fixed bucket creation time always set with now()
    - Creation time for buckets was correctly set on sqlite table but
      when retrieving it, the data was not propagated during the object
      copy chain.
    
    Fixes: https://github.com/aquarist-labs/s3gw/issues/63
    Signed-off-by: Giuseppe Baccini <giuseppe.baccini@suse.com>

--- src/rgw/rgw_sal_sfs.h
+++ src/rgw/rgw_sal_sfs.h
@@ -345,7 +345,7 @@ class SFStore : public Store {
     meta_buckets->store_bucket(info);
 
     sfs::BucketRef b = std::make_shared<sfs::Bucket>(
-      ctx(), this, bucket, owner
+      ctx(), this, info.binfo, owner
     );
     buckets[bucket.name] = b;
     return b;
@@ -364,7 +364,7 @@ class SFStore : public Store {
     for (auto &b : existing) {
       auto user = users.get_user(b.binfo.owner.id);
       sfs::BucketRef ref = std::make_shared<sfs::Bucket>(
-        ctx(), this, b.binfo.bucket, user->uinfo
+        ctx(), this, b.binfo, user->uinfo
       );
       buckets[b.binfo.bucket.name] = ref;
     }
