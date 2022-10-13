c19a5dbda3a62e04603e1516f811613bdac741c2
Marcel Lauhoff <marcel.lauhoff@suse.com>
2022-10-13 17:54:01 +0200

    rgw/status: Add rudimentary SFS status page
    Signed-off-by: Marcel Lauhoff <marcel.lauhoff@suse.com>

--- src/rgw/rgw_sal_sfs.h
+++ src/rgw/rgw_sal_sfs.h
@@ -26,6 +26,7 @@
 #include "rgw_role.h"
 #include "rgw_sal.h"
 
+#include "rgw_status_page.h"
 #include "driver/sfs/types.h"
 #include "driver/sfs/user.h"
 #include "driver/sfs/bucket.h"
@@ -81,6 +82,25 @@ class UnsupportedLuaManager : public StoreLuaManager {
   }
 };
 
+class SFSStatusPage : public StatusPage {
+ private:
+  SFStore *sfs;
+
+ public:
+  SFSStatusPage(SFStore *store);
+  ~SFSStatusPage() override;
+  std::string name() const override {
+    return "SFS";
+  };
+  std::string prefix() const override {
+    return "/sfs";
+  };
+  std::string content_type() const override {
+    return "text/html";
+  };
+  http::status render(std::ostream &os) override;
+};
+
 class SFStore : public StoreDriver {
  private:
   RGWSyncModuleInstanceRef sync_module;
@@ -106,6 +126,10 @@ class SFStore : public StoreDriver {
 
   virtual void finalize(void) override;
   void maybe_init_store();
+
+  std::unique_ptr<StatusPage> make_status_page() {
+    return std::make_unique<SFSStatusPage>(this);
+  }
 
   virtual const std::string get_name() const override { return "sfs"; }
 
@@ -444,6 +468,10 @@ class SFStore : public StoreDriver {
   }
 
   std::string get_cls_name() const { return "sfstore"; }
+
+
+
+  friend class SFSStatusPage;
 };
 
 }  // namespace rgw::sal
