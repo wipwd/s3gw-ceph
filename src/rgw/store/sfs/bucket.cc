653b40876ddc4010fabceccabadd2a521204bf42
Giuseppe Baccini <giuseppe.baccini@suse.com>
2022-10-24 11:26:29 +0200

    rgw/sfs: make SFSBucket::update_container_stats return 0
    This patch allows a client to invoke stat_bucket call.
    The eventual actual implementation will be addressed on a dedicated
    issue.
    
    Fixes: https://github.com/aquarist-labs/s3gw/issues/159
    Signed-off-by: Giuseppe Baccini <giuseppe.baccini@suse.com>

--- src/rgw/store/sfs/bucket.cc
+++ src/rgw/store/sfs/bucket.cc
@@ -381,8 +381,7 @@ int SFSBucket::sync_user_stats(const DoutPrefixProvider *dpp,
   return 0;
 }
 int SFSBucket::update_container_stats(const DoutPrefixProvider *dpp) {
-  ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
-  return -ENOTSUP;
+  return 0;
 }
 int SFSBucket::check_bucket_shards(const DoutPrefixProvider *dpp) {
   ldpp_dout(dpp, 10) << __func__ << ": TODO" << dendl;
