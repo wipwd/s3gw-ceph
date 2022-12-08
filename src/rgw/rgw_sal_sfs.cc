70e22748f37a3bba2d59792f38f2c597ae2fae07
Marcel Lauhoff <marcel.lauhoff@suse.com>
2022-12-08 11:51:47 +0100

    sfs: Add filesystem stats updater service thread
    Periodically update filesystem stats using ceph's statfs wrapper.
    Write values to atomic integers for access by the operation handler
    threads later.
    Configure update interval with `rgw_sfs_stats_update_interval`
    
    Signed-off-by: Marcel Lauhoff <marcel.lauhoff@suse.com>

--- src/rgw/rgw_sal_sfs.cc
+++ src/rgw/rgw_sal_sfs.cc
@@ -20,6 +20,7 @@
 
 #include <sstream>
 #include <system_error>
+#include <thread>
 
 #include "cls/rgw/cls_rgw_client.h"
 #include "common/Clock.h"
@@ -31,6 +32,7 @@
 #include "driver/sfs/writer.h"
 #include "rgw/driver/sfs/sqlite/sqlite_buckets.h"
 #include "rgw/driver/sfs/sqlite/sqlite_users.h"
+#include "include/util.h"
 #include "rgw_acl_s3.h"
 #include "rgw_aio.h"
 #include "rgw_aio_throttle.h"
@@ -478,11 +480,52 @@ void SFStore::maybe_init_store() {
   }
 }
 
+void SFStore::filesystem_stats_updater_main(
+    std::chrono::milliseconds update_interval
+) {
+  while (true) {
+    std::unique_lock lock(filesystem_stats_updater_mutex);
+    ldout(ctx(), 20) << __func__ << ": updating filesystem stats" << dendl;
+
+    ceph_data_stats_t stats;
+    int ret = get_fs_stats(stats, data_path.c_str());
+    ceph_assert(ret >= 0);
+
+    filesystem_stats_avail_bytes = stats.byte_avail;
+    filesystem_stats_avail_percent = stats.avail_percent;
+    filesystem_stats_total_bytes = stats.byte_total;
+
+    const auto shutdown_requested = filesystem_stats_updater_cvar.wait_for(
+        lock, update_interval, [&] { return shutdown; }
+    );
+    if (shutdown_requested) {
+      ldout(ctx(), 10) << __func__ << ": shutting down filesystem stats updater"
+                       << dendl;
+      break;
+    }
+  }
+}
+
 SFStore::SFStore(CephContext* c, const std::filesystem::path& data_path)
-    : sync_module(), zone(this), data_path(data_path), cctx(c) {
+    : sync_module(),
+      zone(this),
+      data_path(data_path),
+      cctx(c),
+      shutdown(false),
+      filesystem_stats_updater_mutex(ceph::make_mutex("sfs:filesystemstats")),
+      filesystem_stats_total_bytes(std::numeric_limits<uint64_t>::max()),
+      filesystem_stats_avail_bytes(std::numeric_limits<uint64_t>::max()),
+      filesystem_stats_avail_percent(100) {
   maybe_init_store();
   db_conn = std::make_shared<sfs::sqlite::DBConn>(cctx);
   gc = std::make_shared<sfs::SFSGC>(cctx, this);
+
+  filesystem_stats_updater = make_named_thread(
+      "sfs_stats_updater", &SFStore::filesystem_stats_updater_main, this,
+      c->_conf.get_val<std::chrono::milliseconds>(
+          "rgw_sfs_stats_update_interval"
+      )
+  );
 
   // no need to be safe, we're in the ctor.
   _refresh_buckets();
@@ -490,7 +533,18 @@ SFStore::SFStore(CephContext* c, const std::filesystem::path& data_path)
   ldout(ctx(), 0) << "sfs serving data from " << data_path << dendl;
 }
 
-SFStore::~SFStore() {}
+SFStore::~SFStore() {
+  shutdown = true;
+
+  if (filesystem_stats_updater.joinable()) {
+#ifdef CEPH_DEBUG_MUTEX
+    filesystem_stats_updater_cvar.notify_all(true);
+#else
+    filesystem_stats_updater_cvar.notify_all();
+#endif  // CEPH_DEBUG_MUTEX
+    filesystem_stats_updater.join();
+  }
+}
 
 }  // namespace rgw::sal
 
