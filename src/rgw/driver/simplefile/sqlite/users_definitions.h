01ba41722e42a822960c7a587df16e8fef3b1ce8
Joao Eduardo Luis <joao@suse.com>
2023-01-14 16:51:34 +0000

    rgw/simplefile: Sqlite users using sqlite_orm library
    Signed-off-by: Xavi Garcia <xavi.garcia@suse.com>
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

---
+++ src/rgw/driver/simplefile/sqlite/users_definitions.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "rgw_common.h"
+
+#include <string>
+
+namespace rgw::sal::simplefile::sqlite  {
+
+using BLOB = std::vector<char>;
+
+// user to be mapped in DB
+// Optinal values mean they might have (or not) a value defined.
+// Blobs are stored as std::vector<char> but we could specialise the encoder and decoder templates
+// from sqlite_orm to store blobs in any user defined type.
+struct DBUser {
+  std::string UserID;
+  std::optional<std::string> Tenant;
+  std::optional<std::string> NS;
+  std::optional<std::string> DisplayName;
+  std::optional<std::string> UserEmail;
+  std::optional<std::string> AccessKeysID;
+  std::optional<std::string> AccessKeysSecret;
+  std::optional<BLOB> AccessKeys;
+  std::optional<BLOB> SwiftKeys;
+  std::optional<BLOB> SubUsers;
+  std::optional<unsigned char> Suspended;
+  std::optional<int> MaxBuckets;
+  std::optional<int> OpMask;
+  std::optional<BLOB> UserCaps;
+  std::optional<int> Admin;
+  std::optional<int> System;
+  std::optional<std::string> PlacementName;
+  std::optional<std::string> PlacementStorageClass;
+  std::optional<BLOB> PlacementTags;
+  std::optional<BLOB> BuckeQuota;
+  std::optional<BLOB> TempURLKeys;
+  std::optional<BLOB> UserQuota;
+  std::optional<int> TYPE;
+  std::optional<BLOB> MfaIDs;
+  std::optional<std::string> AssumedRoleARN;
+  std::optional<BLOB> UserAttrs;
+  std::optional<int> UserVersion;
+  std::optional<std::string> UserVersionTag;
+};
+
+// Struct with information needed by SAL layer
+// Becase sqlite_orm doesn't like nested members like, for instance, uinfo.user_id.id
+// we need to create this structure that will be returned to the user using the SQLiteUsers API.
+// The structure stored and retrieved from the database will be DBUser and the one
+// received and returned by the SQLiteUsers API will be DBOPUserInfo.
+// SQLiteUsers does the needed conversions.
+struct DBOPUserInfo {
+  RGWUserInfo uinfo;
+  obj_version user_version;
+  rgw::sal::Attrs user_attrs;
+};
+
+}  // namespace rgw::sal::simplefile::sqlite
--- src/rgw/rgw_sal_simplefile.h
+++ src/rgw/rgw_sal_simplefile.h
@@ -79,7 +79,6 @@ class UnsupportedLuaManager : public StoreLuaManager {
 
 class SimpleFileStore : public StoreDriver {
  private:
-  RGWUserInfo dummy_user;
   RGWSyncModuleInstanceRef sync_module;
   SimpleFileZone zone;
   const std::filesystem::path data_path;
