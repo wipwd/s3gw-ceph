wipwd/s3gw-ceph

commit f7dbf8bd3d806c8887e42975b56a01e45ccfef5a

rgw/sfs: simplify SFSGC initialization/lifetime
- Lifetime of the GCWorker is now tied to the lifetime of the SFSGC
  object
- The garbagecollector object is created in the constructor of the
  SFStore
- Use smart pointers to manage object lifetime and ensure deallocation
- Remove initialize/finalize and start_/stop_processor methods, as their
  functionality was moved into constructors/destructors
- Prevent the construction of a dysfunctional SFSGC object by replacing
  the default constructor with one that requires all necessary
  parameters

Signed-off-by: Moritz Röhrich <moritz.rohrich@suse.com>

src/rgw/rgw_sal_sfs.cc
@@ -383,16 +383,11 @@ int SFStore::initialize(
   const DoutPrefixProvider* dpp
 ) {
   ldpp_dout(dpp, 10) << __func__ << dendl;
-  gc = new sfs::SFSGC();
-  gc->initialize(cct, this);
-  gc->start_processor();
   return 0;
 }
 
 void SFStore::finalize(void) {
   ldout(ctx(), 10) << __func__ << ": TODO" << dendl;
-  delete gc;
-  gc = nullptr;
   return;
 }
 
@@ -414,6 +409,7 @@ SFStore::SFStore(
 
   maybe_init_store();
   db_conn = std::make_shared<sfs::sqlite::DBConn>(cctx);
+  gc = std::make_shared<sfs::SFSGC>(cctx, this);
 
   // no need to be safe, we're in the ctor.
   _refresh_buckets();

src/rgw/store/sfs/sfs_gc.cc
@@ -19,17 +19,20 @@
 
 namespace rgw::sal::sfs {
 
+SFSGC::SFSGC(CephContext *_cctx, SFStore *_store)
+  : cct(_cctx),
+    store(_store) {
+  worker = std::make_unique<GCWorker>(this, cct, this);
+  worker->create("rgw_gc");
+  down_flag = false;
+}
+
 SFSGC::~SFSGC() {
-  stop_processor();
-  finalize();
-}
-
-void SFSGC::initialize(CephContext *_cct, SFStore *_store) {
-  cct = _cct;
-  store = _store;
-}
-
-void SFSGC::finalize() {
+  down_flag = true;
+  if (worker) {
+    worker->stop();
+    worker->join();
+  }
 }
 
 int SFSGC::process() {
@@ -49,19 +52,16 @@ bool SFSGC::going_down() {
   return down_flag;
 }
 
-void SFSGC::start_processor() {
-  worker = new GCWorker(this, cct, this);
-  worker->create("rgw_gc");
+bool SFSGC::suspended() {
+  return suspend_flag;
 }
 
-void SFSGC::stop_processor() {
-  down_flag = true;
-  if (worker) {
-    worker->stop();
-    worker->join();
-    delete worker;
-  }
-  worker = nullptr;
+void SFSGC::suspend() {
+  suspend_flag = true;
+}
+
+void SFSGC::resume() {
+  suspend_flag = false;
 }
 
 unsigned SFSGC::get_subsys() const
@@ -161,14 +161,18 @@ void *SFSGC::GCWorker::entry() {
   do {
     utime_t start = ceph_clock_now();
     lsfs_dout(dpp, 2) << "start" << dendl;
-    int r = gc->process();
-    if (r < 0) {
-      lsfs_dout(dpp, 0)
-         << "ERROR: garbage collection process() returned error r="
-         << r
-         << dendl;
+
+    if (!gc->suspended()) {
+      int r = gc->process();
+      if (r < 0) {
+        lsfs_dout(dpp, 0)
+           << "ERROR: garbage collection process() returned error r="
+           << r
+           << dendl;
+      }
+      lsfs_dout(dpp, 2) << "stop" << dendl;
+
     }
-    lsfs_dout(dpp, 2) << "stop" << dendl;
 
     if (gc->going_down())
       break;

src/rgw/store/sfs/sfs_gc.h
@@ -13,6 +13,8 @@
  */
 #pragma once
 
+#include <memory>
+
 #include "rgw_sal.h"
 #include "rgw_sal_sfs.h"
 #include "store/sfs/types.h"
@@ -22,7 +24,8 @@ namespace rgw::sal::sfs {
 class SFSGC : public DoutPrefixProvider {
   CephContext *cct = nullptr;
   SFStore *store = nullptr;
-  std::atomic<bool> down_flag = { false };
+  std::atomic<bool> down_flag = { true };
+  std::atomic<bool> suspend_flag = { false };
   long int max_objects;
 
   class GCWorker : public Thread {
@@ -36,23 +39,22 @@ class SFSGC : public DoutPrefixProvider {
 
   public:
     GCWorker(const DoutPrefixProvider *_dpp, CephContext *_cct, SFSGC *_gc);
+
     void *entry() override;
     void stop();
   };
 
-  GCWorker *worker = nullptr;
+  std::unique_ptr<GCWorker> worker = nullptr;
 public:
-  SFSGC() = default;
+  SFSGC(CephContext *, SFStore *);
   ~SFSGC();
-
-  void initialize(CephContext *_cct, SFStore *_store);
-  void finalize();
 
   int process();
 
   bool going_down();
-  void start_processor();
-  void stop_processor();
+  bool suspended();
+  void suspend();
+  void resume();
 
   CephContext *get_cct() const override { return store->ctx(); }
   unsigned get_subsys() const;

src/rgw/rgw_sal_sfs.h
@@ -44,6 +44,7 @@
 namespace rgw::sal::sfs {
   class SFSGC;
 }
+
 namespace rgw::sal {
 
 class SFStore;
@@ -78,10 +79,10 @@ class SFStore : public Store {
   CephContext *const cctx;
   ceph::mutex buckets_map_lock = ceph::make_mutex("buckets_map_lock");
   std::map<std::string, sfs::BucketRef> buckets;
-  sfs::SFSGC *gc = nullptr;
 
  public:
   sfs::sqlite::DBConnRef db_conn;
+  std::shared_ptr<sfs::SFSGC> gc = nullptr;
 
   SFStore(CephContext *c, const std::filesystem::path &data_path);
   SFStore(const SFStore&) = delete;

src/test/rgw/sfs/test_rgw_sfs_gc.cc
@@ -175,6 +175,8 @@ TEST_F(TestSFSGC, TestDeletedBuckets) {
   ceph_context->_conf.set_val("rgw_sfs_data_path", getTestDir());
   ceph_context->_conf.set_val("rgw_gc_processor_period", "1");
   auto store = new rgw::sal::SFStore(ceph_context.get(), getTestDir());
+  auto gc = store->gc;
+  gc->suspend();  // start suspended so we have control over processing
 
   NoDoutPrefix ndp(ceph_context.get(), 1);
   RGWEnv env;
@@ -201,11 +203,6 @@ TEST_F(TestSFSGC, TestDeletedBuckets) {
   // we should have 5 version files plus the sqlite db
   EXPECT_EQ(getStoreNbFiles(), 6);
 
-  auto gc = std::make_shared<rgw::sal::sfs::SFSGC>();
-  gc->initialize(ceph_context.get(), store);
-  gc->start_processor();
-  gc->stop_processor();
-
   // nothing should be removed
   EXPECT_EQ(getStoreNbFiles(), 6);
   SQLiteVersionedObjects db_versioned_objs(store->db_conn);
@@ -220,8 +217,7 @@ TEST_F(TestSFSGC, TestDeletedBuckets) {
   // we should have 1 more version (delete marker for 1 object)
   EXPECT_EQ(versions.size(), 6);
 
-  gc->start_processor();
-  gc->stop_processor();
+  gc->process();
 
   // only objects for bucket 1 should be available
   EXPECT_EQ(getStoreNbFiles(), 4);
@@ -232,8 +228,7 @@ TEST_F(TestSFSGC, TestDeletedBuckets) {
 
   // delete bucket 1 now
   deleteTestBucket("test_bucket_1", store->db_conn);
-  gc->start_processor();
-  gc->stop_processor();
+  gc->process();
 
   // only the db file should be present
   EXPECT_EQ(getStoreNbFiles(), 1);
@@ -250,6 +245,8 @@ TEST_F(TestSFSGC, TestDeletedBucketsMaxObjects) {
   // gc will only remove 1 objects per iteration
   ceph_context->_conf.set_val("rgw_gc_max_objs", "1");
   auto store = new rgw::sal::SFStore(ceph_context.get(), getTestDir());
+  auto gc = store->gc;
+  gc->suspend();
 
   NoDoutPrefix ndp(ceph_context.get(), 1);
   RGWEnv env;
@@ -276,14 +273,10 @@ TEST_F(TestSFSGC, TestDeletedBucketsMaxObjects) {
   // we should have 5 version files plus the sqlite db
   EXPECT_EQ(getStoreNbFiles(), 6);
 
-  auto gc = std::make_shared<rgw::sal::sfs::SFSGC>();
-  gc->initialize(ceph_context.get(), store);
-
   // delete bucket 2
   deleteTestBucket("test_bucket_2", store->db_conn);
 
-  gc->start_processor();
-  gc->stop_processor();
+  gc->process();
 
   // only 1 file should be removed
   EXPECT_EQ(getStoreNbFiles(), 5);
@@ -291,8 +284,7 @@ TEST_F(TestSFSGC, TestDeletedBucketsMaxObjects) {
   EXPECT_EQ(1, getNumberObjectsForBucket("test_bucket_2", store->db_conn));
   EXPECT_EQ(1, getNumberObjectsForBucket("test_bucket_1", store->db_conn));
 
-  gc->start_processor();
-  gc->stop_processor();
+  gc->process();
 
   // one more version removed
   EXPECT_EQ(getStoreNbFiles(), 4);
@@ -300,15 +292,13 @@ TEST_F(TestSFSGC, TestDeletedBucketsMaxObjects) {
   EXPECT_EQ(1, getNumberObjectsForBucket("test_bucket_2", store->db_conn));
   EXPECT_EQ(1, getNumberObjectsForBucket("test_bucket_1", store->db_conn));
 
-  gc->start_processor();
-  gc->stop_processor();
+  gc->process();
   // one more version removed
   EXPECT_EQ(getStoreNbFiles(), 4);
   EXPECT_EQ(1, getNumberObjectsForBucket("test_bucket_2", store->db_conn));
   EXPECT_EQ(1, getNumberObjectsForBucket("test_bucket_1", store->db_conn));
 
-  gc->start_processor();
-  gc->stop_processor();
+  gc->process();
   // one more version removed
   EXPECT_EQ(getStoreNbFiles(), 4);
   // the object is finally removed

commit 9bd8686b94f34518d0ce80a4b52123e5ffdf2655

rgw: fix order of initialization and use in SFSGC
Fix the order of initialization and use of the SFSGC. The worker thread
can not start running until the SFSGC object has been completely
allocated, because the SFSGC object is the dout prefix provider and a
pointer to the SFSGC instance would be invalid until the constructor has
finished.
Since the worker thread might want to print something out, this would
resuult in an invalid pointer dereference.

Upon stopping, the SFSGC must query the worker thread if it has started,
because an SFSGC instance could be created without calling
`SFSGC::initialize()`. In this case calling `join()` on the worker has
to be prevented.

Fixes bugs introduced in:
f7dbf8bd3d806c8887e42975b56a01e45ccfef5a

Signed-off-by: Moritz Röhrich <moritz.rohrich@suse.com>

src/rgw/rgw_sal_sfs.cc
@@ -459,6 +459,7 @@ int SFStore::initialize(
   const DoutPrefixProvider* dpp
 ) {
   ldpp_dout(dpp, 10) << __func__ << dendl;
+  gc->initialize();
   return 0;
 }
 

src/rgw/store/sfs/sfs_gc.cc
@@ -15,7 +15,6 @@
 
 #include "store/sfs/sqlite/sqlite_objects.h"
 
-#define dout_subsys ceph_subsys_rgw
 
 namespace rgw::sal::sfs {
 
@@ -23,13 +22,11 @@ SFSGC::SFSGC(CephContext *_cctx, SFStore *_store)
   : cct(_cctx),
     store(_store) {
   worker = std::make_unique<GCWorker>(this, cct, this);
-  worker->create("rgw_gc");
-  down_flag = false;
 }
 
 SFSGC::~SFSGC() {
   down_flag = true;
-  if (worker) {
+  if (worker->is_started()) {
     worker->stop();
     worker->join();
   }
@@ -52,6 +49,16 @@ bool SFSGC::going_down() {
   return down_flag;
 }
 
+/*
+ * The constructor must have finished before the worker thread can be created,
+ * because otherwise the logging will dereference an invalid pointer, since the
+ * SFSGC instance is a prefix provider for the logging in the worker thread
+ */
+void SFSGC::initialize() {
+  worker->create("rgw_gc");
+  down_flag = false;
+}
+
 bool SFSGC::suspended() {
   return suspend_flag;
 }
@@ -62,11 +69,6 @@ void SFSGC::suspend() {
 
 void SFSGC::resume() {
   suspend_flag = false;
-}
-
-unsigned SFSGC::get_subsys() const
-{
-  return dout_subsys;
 }
 
 std::ostream& SFSGC::gen_prefix(std::ostream& out) const

src/rgw/store/sfs/sfs_gc.h
@@ -19,6 +19,9 @@
 #include "rgw_sal_sfs.h"
 #include "store/sfs/types.h"
 
+#define sfs_dout_subsys ceph_subsys_rgw
+
+
 namespace rgw::sal::sfs {
 
 class SFSGC : public DoutPrefixProvider {
@@ -52,14 +55,15 @@ public:
   int process();
 
   bool going_down();
+  void initialize();
   bool suspended();
   void suspend();
   void resume();
 
   CephContext *get_cct() const override { return store->ctx(); }
-  unsigned get_subsys() const;
+  unsigned get_subsys() const override { return sfs_dout_subsys; }
 
-  std::ostream& gen_prefix(std::ostream& out) const;
+  std::ostream& gen_prefix(std::ostream& out) const override;
 
   std::string get_cls_name() const { return "SFSGC"; }
 