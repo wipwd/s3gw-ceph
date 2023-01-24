jecluis/ceph

rgw/sfs: make sqlite play nice multithreaded
Because sqlite is essentially a one-file database, we can't be reading
while writing, nor having multiple parallel writes. However, we can
have any number of parallel reads. Therefore, we now rely on a shared
mutex to gate reads and writes.

Additionally, we now keep the database open forever, and maintain one
single database connection, instead of one connection per operation.
This allows us finer grained control over accesses.

Signed-off-by: Joao Eduardo Luis <joao@suse.com>

src/rgw/rgw_sal_sfs.cc
@@ -257,7 +257,7 @@ int SFStore::meta_list_keys_next(const DoutPrefixProvider *dpp,
                                          void *handle, int max,
                                          list<string> &keys, bool *truncated) {
   *truncated = false;
-  rgw::sal::sfs::sqlite::SQLiteUsers sqlite_users(dpp->get_cct());
+  rgw::sal::sfs::sqlite::SQLiteUsers sqlite_users(conn);
   auto ids = sqlite_users.get_user_ids();
   std::copy(ids.begin(), ids.end(), std::back_inserter(keys));
   return 0;
@@ -396,9 +396,9 @@ SFStore::SFStore(
     cctx(c) {
 
   maybe_init_store();
+  conn = std::make_shared<sfs::sqlite::DBConn>(cctx);
   init_buckets();
 
-  meta_buckets = sfs::get_meta_buckets(c);
   // no need to be safe, we're in the ctor.
   _refresh_buckets();
 

wipwd/s3gw-ceph

src/rgw/rgw_sal_sfs.cc
@@ -275,7 +275,7 @@ int SFStore::meta_list_keys_next(const DoutPrefixProvider *dpp,
                                          void *handle, int max,
                                          list<string> &keys, bool *truncated) {
   *truncated = false;
-  rgw::sal::sfs::sqlite::SQLiteUsers sqlite_users(dpp->get_cct());
+  rgw::sal::sfs::sqlite::SQLiteUsers sqlite_users(db_conn);
   auto ids = sqlite_users.get_user_ids();
   std::copy(ids.begin(), ids.end(), std::back_inserter(keys));
   return 0;
@@ -422,9 +422,9 @@ SFStore::SFStore(
     cctx(c) {
 
   maybe_init_store();
+  db_conn = std::make_shared<sfs::sqlite::DBConn>(cctx);
   init_buckets();
 
-  meta_buckets = sfs::get_meta_buckets(c);
   // no need to be safe, we're in the ctor.
   _refresh_buckets();
 