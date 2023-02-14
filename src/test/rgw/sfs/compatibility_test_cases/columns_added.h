42690d77b9099ee77ac32e4e696c491da0a91d4f
Marcel Lauhoff <marcel.lauhoff@suse.com>
2023-02-14 08:20:01 +0100

    tests/sfs: set up TestDB with sqlite WAL mode
    Set up sqlite in TestDBs like we do in the SFS DBConn.
    
    Signed-off-by: Marcel Lauhoff <marcel.lauhoff@suse.com>

--- src/test/rgw/sfs/compatibility_test_cases/columns_added.h
+++ src/test/rgw/sfs/compatibility_test_cases/columns_added.h
@@ -186,6 +186,14 @@ struct TestDB {
   explicit TestDB(const std::string & db_full_path)
     : storage(_make_test_storage(db_full_path))
   {
+    storage.on_open = [](sqlite3* db) {
+      sqlite3_extended_result_codes(db, 1);
+      sqlite3_busy_timeout(db, 10000);
+      sqlite3_exec(db,
+                   "PRAGMA journal_mode=WAL;PRAGMA synchronous=normal;"
+                   "PRAGMA temp_store = memory;PRAGMA mmap_size = 30000000000;",
+                   0, 0, 0);
+    };
     storage.open_forever();
     storage.busy_timeout(5000);
     storage.sync_schema();
