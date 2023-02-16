wipwd/s3gw-ceph

commit d687f401cbc7de040130c1fbb639ded531e521e0

sfs: Add filesystem stats updater service thread
Periodically update filesystem stats using ceph's statfs wrapper.
Write values to atomic integers for access by the operation handler
threads later.
Configure update interval with `rgw_sfs_stats_update_interval`

Signed-off-by: Marcel Lauhoff <marcel.lauhoff@suse.com>

src/rgw/rgw_sal_sfs.cc
@@ -20,11 +20,13 @@
 
 #include <sstream>
 #include <system_error>
+#include <thread>
 
 #include "cls/rgw/cls_rgw_client.h"
 #include "common/Clock.h"
 #include "common/ceph_mutex.h"
 #include "common/errno.h"
+#include "include/util.h"
 #include "rgw_acl_s3.h"
 #include "rgw_aio.h"
 #include "rgw_aio_throttle.h"
@@ -502,17 +504,48 @@ void SFStore::maybe_init_store() {
 
 }
 
-SFStore::SFStore(
-  CephContext *c,
-  const std::filesystem::path &data_path
-) : sync_module(),
-    zone(this),
-    data_path(data_path),
-    cctx(c) {
+void SFStore::filesystem_stats_updater_main(
+    std::chrono::milliseconds update_interval) {
+  while (true) {
+    std::unique_lock lock(filesystem_stats_updater_mutex);
+    ldout(ctx(), 20) << __func__ << ": updating filesystem stats" << dendl;
 
+    ceph_data_stats_t stats;
+    int ret = get_fs_stats(stats, data_path.c_str());
+    ceph_assert(ret >= 0);
+
+    filesystem_stats_avail_bytes = stats.byte_avail;
+    filesystem_stats_avail_percent = stats.avail_percent;
+    filesystem_stats_total_bytes = stats.byte_total;
+
+    const auto shutdown_requested = filesystem_stats_updater_cvar.wait_for(
+        lock, update_interval, [&] { return shutdown; });
+    if (shutdown_requested) {
+      ldout(ctx(), 10) << __func__ << ": shutting down filesystem stats updater"
+                       << dendl;
+      break;
+    }
+  }
+}
+
+SFStore::SFStore(CephContext* c, const std::filesystem::path& data_path)
+    : sync_module(),
+      zone(this),
+      data_path(data_path),
+      cctx(c),
+      shutdown(false),
+      filesystem_stats_updater_mutex(ceph::make_mutex("sfs:filesystemstats")),
+      filesystem_stats_total_bytes(std::numeric_limits<uint64_t>::max()),
+      filesystem_stats_avail_bytes(std::numeric_limits<uint64_t>::max()),
+      filesystem_stats_avail_percent(100) {
   maybe_init_store();
   db_conn = std::make_shared<sfs::sqlite::DBConn>(cctx);
   gc = std::make_shared<sfs::SFSGC>(cctx, this);
+
+  filesystem_stats_updater = make_named_thread(
+      "sfs_stats_updater", &SFStore::filesystem_stats_updater_main, this,
+      c->_conf.get_val<std::chrono::milliseconds>(
+          "rgw_sfs_stats_update_interval"));
 
   // no need to be safe, we're in the ctor.
   _refresh_buckets();
@@ -520,7 +553,14 @@ SFStore::SFStore(
   ldout(ctx(), 0) << "sfs serving data from " << data_path << dendl;
 }
 
-SFStore::~SFStore() { }
+SFStore::~SFStore() {
+  shutdown = true;
+
+  if (filesystem_stats_updater.joinable()) {
+    filesystem_stats_updater_cvar.notify_all(true);
+    filesystem_stats_updater.join();
+  }
+}
 
 }  // namespace rgw::sal
 

src/common/options/rgw.yaml.in
@@ -3729,3 +3729,10 @@ options:
   desc: RGW SFS store data dir
   service:
     - rgw
+- name: rgw_sfs_stats_update_interval
+  type: millisecs
+  level: advanced
+  default: 10000
+  desc: RGW SFS stats update interval in milliseconds
+  service:
+    - rgw

src/rgw/rgw_sal_sfs.h
@@ -16,8 +16,11 @@
 #ifndef RGW_STORE_SFS_H
 #define RGW_STORE_SFS_H
 
+#include <atomic>
+#include <chrono>
 #include <filesystem>
 
+#include "common/ceph_mutex.h"
 #include "rgw_multi.h"
 #include "rgw_notify.h"
 #include "rgw_oidc_provider.h"
@@ -111,9 +114,20 @@ class SFStore : public StoreDriver {
   ceph::mutex buckets_map_lock = ceph::make_mutex("buckets_map_lock");
   std::map<std::string, sfs::BucketRef> buckets;
 
+  // Signal shutdown condition to service threads
+  bool shutdown;
+
+  std::thread filesystem_stats_updater;
+  ceph::condition_variable filesystem_stats_updater_cvar;
+  ceph::mutex filesystem_stats_updater_mutex;
+
  public:
   sfs::sqlite::DBConnRef db_conn;
   std::shared_ptr<sfs::SFSGC> gc = nullptr;
+
+  std::atomic_uint64_t filesystem_stats_total_bytes;
+  std::atomic_uint64_t filesystem_stats_avail_bytes;
+  std::atomic_uint64_t filesystem_stats_avail_percent;
 
   SFStore(CephContext *c, const std::filesystem::path &data_path);
   SFStore(const SFStore&) = delete;
@@ -131,6 +145,8 @@ class SFStore : public StoreDriver {
   std::unique_ptr<StatusPage> make_status_page() {
     return std::make_unique<SFSStatusPage>(this);
   }
+
+  void filesystem_stats_updater_main(std::chrono::milliseconds update_interval);
 
   virtual const std::string get_name() const override { return "sfs"; }
 

commit ba88fc32bf70ba028213f493414c7d837519241c

rgw/sfs: fix condition variable in release builds
The ceph::condition_variable.notify_all() method takes an optional
boolean parameter. For release builds,
std::condition_variable.notify_all() is used instead, which doesn't take
any parameters. This #ifdef guards the use of the debug variant and
restricts is usage to debug builds, allowing release builds to proceed.

fixes build-failure introduced in: d687f401cbc7

Signed-off-by: Moritz Röhrich <moritz.rohrich@suse.com>

src/rgw/rgw_sal_sfs.cc
@@ -559,7 +559,11 @@ SFStore::~SFStore() {
   shutdown = true;
 
   if (filesystem_stats_updater.joinable()) {
+#ifdef CEPH_DEBUG_MUTEX
     filesystem_stats_updater_cvar.notify_all(true);
+#else
+    filesystem_stats_updater_cvar.notify_all();
+#endif  // CEPH_DEBUG_MUTEX
     filesystem_stats_updater.join();
   }
 }