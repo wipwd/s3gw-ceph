2c5f66d013c79d747d29903dc7b0b979aa2858af
Marcel Lauhoff <marcel.lauhoff@suse.com>
2023-08-18 15:20:24 +0200

    rgw/sfs: Use MP id rather than part num in GC
    Signed-off-by: Marcel Lauhoff <marcel.lauhoff@suse.com>

--- src/rgw/driver/sfs/sqlite/sqlite_multipart.cc
+++ src/rgw/driver/sfs/sqlite/sqlite_multipart.cc
@@ -462,7 +462,7 @@ SQLiteMultipart::remove_multiparts_by_bucket_id_transact(
     ret_parts = storage.select(
         columns(
             &DBMultipart::upload_id, &DBMultipart::object_uuid,
-            &DBMultipartPart::part_num
+            &DBMultipartPart::id
         ),
         inner_join<DBMultipart>(
             on(is_equal(&DBMultipart::upload_id, &DBMultipartPart::upload_id))
