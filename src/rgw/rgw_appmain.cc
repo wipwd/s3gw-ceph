wipwd/s3gw-ceph

commit 9b02b73a9ea6cafa3d4151951d7c7917a1ba597d

rgw/s3gw: Add telemetry / upgrade responder service
Add S3GWTelemetry "service" - an object sending periodic requests to
our upgrade responder instance and keeping the response for later
consumption.

Add status page '/telemetry' exporting upgrade responder request,
S3GWTelemetry status information and the available versions.

Example request (devel build):
```
{
    "appVersion": "18.0.0-3118-ge21d2ab4b05",
    "extraInfo": {
        "ceph_version": "ceph version 18.0.0-3118-ge21d2ab4b05 (e21d2ab4b0581a3aaeacaab2ef597ab41356b243) reef (dev)",
        "ceph_version_short": "18.0.0-3118-ge21d2ab4b05",
        "ceph_release": "reef",
        "os": "Linux",
        "kernel_version": "6.1.7-1-default",
        "kernel_description": "#1 SMP PREEMPT_DYNAMIC Wed Jan 18 11:12:34 UTC 2023 (872045c)",
        "arch": "x86_64",
        "mem_total_kb": "32486064",
        "mem_swap_kb": "32485372",
        "cpu": "Intel(R) Core(TM) i9-10885H CPU @ 2.40GHz",
        "cgroup_mem_limit": "0",
        "sfs_avail_kb": 43152412,
        "sfs_total_kb": 300933564,
        "sfs_used_kb": 257781152,
        "sfs_fs": "xfs"
    }
}
```

Signed-off-by: Marcel Lauhoff <marcel.lauhoff@suse.com>

src/rgw/rgw_appmain.cc
@@ -15,6 +15,7 @@
 
 #include <boost/intrusive/list.hpp>
 #include <memory>
+#include "global/global_context.h"
 #include "global/global_init.h"
 #include "global/signal_handler.h"
 #include "common/config.h"
@@ -28,6 +29,7 @@
 #include "include/stringify.h"
 #include "rgw_main.h"
 #include "rgw_common.h"
+#include "rgw_s3gw_telemetry.h"
 #include "rgw_sal_rados.h"
 #include "rgw_period_pusher.h"
 #include "rgw_realm_reloader.h"
@@ -43,6 +45,7 @@
 #include "rgw_rest_config.h"
 #include "rgw_rest_realm.h"
 #include "rgw_rest_ratelimit.h"
+#include "rgw_status_page_telemetry.h"
 #include "rgw_swift_auth.h"
 #include "rgw_log.h"
 #include "rgw_lib.h"
@@ -463,6 +466,10 @@ int rgw::AppMain::init_frontends2(RGWLib* rgwlib)
           cct->get_perfcounters_collection()
         )
       );
+      if (env.s3gw_telemetry) {
+	stat->register_status_page(
+	    std::make_unique<TelemetryStatusPage>(cct, *env.s3gw_telemetry));
+      }
       auto driver = env.driver;
       if (driver && driver->get_name() == "sfs") {
         auto sfs = dynamic_cast<rgw::sal::SFStore*>(driver);
@@ -580,6 +587,17 @@ void rgw::AppMain::init_lua()
   }
 } /* init_lua */
 
+void rgw::AppMain::init_s3gw_telemetry()
+{
+  rgw::sal::Driver* driver = env.driver;
+  if (driver->get_name() == "sfs") { 
+    rgw::sal::SFStore* sfs = static_cast<rgw::sal::SFStore*>(env.driver);
+    env.s3gw_telemetry.reset(new S3GWTelemetry(g_ceph_context, sfs));
+    env.s3gw_telemetry->start();
+  }
+} /* init_s3gw_telemetry */
+
+
 void rgw::AppMain::shutdown(std::function<void(void)> finalize_async_signals)
 {
   if (env.driver->get_name() == "rados") {
@@ -626,4 +644,5 @@ void rgw::AppMain::shutdown(std::function<void(void)> finalize_async_signals)
 #endif
   rgw_perf_stop(g_ceph_context);
   ratelimiter.reset(); // deletes--ensure this happens before we destruct
+  env.s3gw_telemetry->stop();
 } /* AppMain::shutdown */

src/common/options/rgw.yaml.in
@@ -3788,3 +3788,18 @@ options:
     Reserves space for metadata operations and SQLite updates.
   service:
     - rgw
+- name: rgw_s3gw_telemetry_update_interval
+  type: millisecs
+  level: advanced
+  default: 3600000
+  desc: S3GW telemetry update inverval. Set to 0 to disable.
+  service:
+    - rgw
+- name: rgw_s3gw_telemetry_upgrade_responder_url
+  type: str
+  level: advanced
+  default: https://s3gw.version.rancher.io/v1/checkupgrade
+  desc:
+    Rancher upgrade responder URL
+  service:
+    - rgw

src/rgw/CMakeLists.txt
@@ -148,6 +148,7 @@ set(librgw_common_srcs
   rgw_bucket_encryption.cc
   rgw_tracer.cc
   rgw_lua_background.cc
+  rgw_s3gw_telemetry.cc
   driver/rados/cls_fifo_legacy.cc
   driver/rados/rgw_bucket.cc
   driver/rados/rgw_bucket_sync.cc
@@ -369,6 +370,7 @@ set(rgw_a_srcs
   rgw_signal.cc
   rgw_status_frontend.cc
   rgw_status_page.cc
+  rgw_status_page_telemetry.cc
   rgw_swift_auth.cc
   rgw_usage.cc
   rgw_sts.cc

src/rgw/rgw_main.cc
@@ -146,6 +146,7 @@ int main(int argc, char *argv[])
     return EIO;
   }
 
+  main.init_s3gw_telemetry();
   main.cond_init_apis();
 
   mutex.lock();

src/rgw/rgw_main.h
@@ -107,6 +107,7 @@ public:
   void init_tracepoints();
   void init_notification_endpoints();
   void init_lua();
+  void init_s3gw_telemetry();
 
   bool have_http() {
     return have_http_frontend;

src/rgw/rgw_process_env.h
@@ -4,6 +4,7 @@
 #pragma once
 
 #include <memory>
+#include "rgw_s3gw_telemetry.h"
 
 class ActiveRateLimiter;
 class OpsLogSink;
@@ -40,6 +41,7 @@ struct RGWProcessEnv {
   OpsLogSink *olog = nullptr;
   std::unique_ptr<rgw::auth::StrategyRegistry> auth_registry;
   ActiveRateLimiter* ratelimiting = nullptr;
+  std::unique_ptr<S3GWTelemetry> s3gw_telemetry = nullptr;
 
 #ifdef WITH_ARROW_FLIGHT
   // managed by rgw:flight::FlightFrontend in rgw_flight_frontend.cc

src/rgw/rgw_s3gw_telemetry.cc
@@ -1,0 +1,326 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=2 sw=2 expandtab ft=cpp
+
+/*
+ * Ceph - scalable distributed file system
+ * SFS SAL implementation
+ *
+ * Copyright (C) 2023 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ *
+ */
+
+#include "rgw_s3gw_telemetry.h"
+
+#include <asm-generic/errno-base.h>
+#include <curl/curl.h>
+
+#include <boost/beast/http/status.hpp>
+#include <memory>
+#include <thread>
+
+#include "common/Formatter.h"
+#include "common/Thread.h"
+#include "common/ceph_context.h"
+#include "common/ceph_json.h"
+#include "common/ceph_time.h"
+#include "common/dout.h"
+#include "common/version.h"
+#include "global/global_context.h"
+#include "include/buffer_fwd.h"
+#include "include/util.h"
+
+#if defined(__linux__)
+#include <linux/magic.h>
+#include <sys/vfs.h>
+#endif
+
+#define dout_subsys ceph_subsys_rgw
+
+S3GWTelemetry::S3GWTelemetry(
+    CephContext* _cct, const rgw::sal::SFStore* const _sfs
+)
+    : m_state(),
+      m_cct(_cct),
+      m_sfs(_sfs),
+      m_shutdown(true),
+      m_updater_cvar(),
+      m_updater_mutex(make_mutex("S3GWTelemetry::Updater")) {}
+
+void S3GWTelemetry::start() {
+  std::chrono::milliseconds interval =
+      m_cct->_conf.get_val<std::chrono::milliseconds>(
+          "rgw_s3gw_telemetry_update_interval"
+      );
+
+  if (interval.count() == 0) {
+    ldout(
+        m_cct, 1
+    ) << __func__
+      << ": telemetry update interval set to 0. telemetry disabled." << dendl;
+  } else {
+    m_shutdown = false;
+    m_updater = make_named_thread(
+        "s3gw_telemetry_updater", &S3GWTelemetry::updater_main, this,
+        m_cct->_conf.get_val<std::chrono::milliseconds>(
+            "rgw_s3gw_telemetry_update_interval"
+        )
+    );
+  }
+}
+
+void S3GWTelemetry::wake_up() {
+#ifdef CEPH_DEBUG_MUTEX
+  m_updater_cvar.notify_all(true);
+#else
+  m_updater_cvar.notify_all();
+#endif  // CEPH_DEBUG_MUTEX
+}
+
+void S3GWTelemetry::stop() {
+  m_shutdown = true;
+  wake_up();
+  m_updater.join();
+}
+
+void S3GWTelemetry::update() {
+  wake_up();
+}
+
+void S3GWTelemetry::updater_main(std::chrono::milliseconds update_interval) {
+  while (true) {
+    std::unique_lock lock(m_updater_mutex);
+    ldout(m_cct, 19) << __func__ << ": updating telemetry" << dendl;
+    do_update();
+    const auto shutdown_requested = m_updater_cvar.wait_for(
+        lock, update_interval, [&] { return m_shutdown; }
+    );
+    if (shutdown_requested) {
+      ldout(m_cct, 10) << __func__ << ": shutting down telemetry updater"
+                       << dendl;
+      break;
+    }
+  }
+}
+
+S3GWTelemetry::Status S3GWTelemetry::status() const {
+  return m_state.status();
+}
+
+std::vector<S3GWTelemetry::Version> S3GWTelemetry::available_versions() const {
+  return m_state.versions();
+}
+
+bool S3GWTelemetry::post_to_update_responder(
+    const std::string& body, bufferlist& response
+) const {
+  auto curl = std::unique_ptr<CURL, decltype(&curl_easy_cleanup)>(
+      curl_easy_init(), curl_easy_cleanup
+  );
+  if (!curl) {
+    return false;
+  }
+  const auto headers =
+      std::unique_ptr<struct curl_slist, decltype(&curl_slist_free_all)>(
+          []() {
+            struct curl_slist* result =
+                curl_slist_append(nullptr, "Accept: application/json");
+            result =
+                curl_slist_append(result, "Content-Type: application/json");
+            result = curl_slist_append(result, "Expect:");
+            return result;
+          }(),
+          curl_slist_free_all
+      );
+  const auto url = m_cct->_conf.get_val<std::string>(
+      "rgw_s3gw_telemetry_upgrade_responder_url"
+  );
+
+  curl_easy_setopt(curl.get(), CURLOPT_URL, url.c_str());
+  curl_easy_setopt(curl.get(), CURLOPT_POST, 1L);
+  curl_easy_setopt(curl.get(), CURLOPT_HTTPHEADER, headers.get());
+  curl_easy_setopt(curl.get(), CURLOPT_READDATA, &body);
+  curl_easy_setopt(
+      curl.get(), CURLOPT_READFUNCTION,
+      +[](char* buffer, size_t size, size_t nmemb, void* userdata) -> size_t {
+        const std::string* body = static_cast<const std::string*>(userdata);
+        const size_t bytes_to_copy = size * nmemb;
+        const size_t bytes_copied = body->copy(buffer, bytes_to_copy);
+        return bytes_copied;
+      }
+  );
+  curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &response);
+  curl_easy_setopt(
+      curl.get(), CURLOPT_WRITEFUNCTION,
+      +[](char* buffer, size_t size, size_t nmemb, void* userdata) -> size_t {
+        bufferlist* response = static_cast<bufferlist*>(userdata);
+        const size_t bytes_to_copy = size * nmemb;
+        response->append(buffer, bytes_to_copy);
+        return bytes_to_copy;
+      }
+  );
+  curl_easy_setopt(curl.get(), CURLOPT_POSTFIELDSIZE, body.length());
+  curl_easy_setopt(curl.get(), CURLOPT_VERBOSE, 0L);
+  curl_easy_setopt(curl.get(), CURLOPT_TIMEOUT, 10);
+  curl_easy_setopt(curl.get(), CURLOPT_NOPROGRESS, 1L);
+  curl_easy_setopt(curl.get(), CURLOPT_NOSIGNAL, 1L);
+  curl_easy_setopt(
+      curl.get(), CURLOPT_LOW_SPEED_TIME, m_cct->_conf->rgw_curl_low_speed_time
+  );
+  curl_easy_setopt(
+      curl.get(), CURLOPT_LOW_SPEED_LIMIT,
+      m_cct->_conf->rgw_curl_low_speed_limit
+  );
+  curl_easy_setopt(
+      curl.get(), CURLOPT_TCP_KEEPALIVE, m_cct->_conf->rgw_curl_tcp_keepalive
+  );
+  curl_easy_setopt(
+      curl.get(), CURLOPT_BUFFERSIZE, m_cct->_conf->rgw_curl_buffersize
+  );
+
+  CURLcode res = curl_easy_perform(curl.get());
+
+  if (res != CURLE_OK) {
+    long http_status;
+    curl_easy_getinfo(curl.get(), CURLINFO_RESPONSE_CODE, &http_status);
+    ldout(m_cct, 2) << __func__ << ": upgrade responder POST unsuccessful. "
+                    << " http status: " << http_status << dendl;
+  }
+
+  return (res == CURLE_OK);
+}
+
+void S3GWTelemetry::Version::decode_json(JSONObj* obj) {
+  JSONDecoder::decode_json("Name", name, obj, true);
+  JSONDecoder::decode_json("ReleaseDate", release_date, obj, true);
+}
+
+std::optional<std::vector<S3GWTelemetry::Version>>
+S3GWTelemetry::parse_upgrade_response(bufferlist& response) const {
+  std::vector<Version> result;
+  JSONParser parser;
+  if (!parser.parse(response.c_str(), response.length())) {
+    ldout(m_cct, 2) << __func__ << ": failed to parse update responder JSON."
+                    << dendl;
+    ldout(m_cct, 20) << __func__ << ": response data was: " << response.to_str()
+                     << dendl;
+    return std::nullopt;
+  }
+
+  auto versions_iter = parser.find_first("versions");
+  if (versions_iter.end()) {
+    ldout(m_cct, 2) << __func__ << ": failed to decode update responder JSON. "
+                    << " no versions object found" << dendl;
+    return std::nullopt;
+  }
+
+  JSONObj* versions_json = *versions_iter;
+  auto version_iter = versions_json->find_first();
+  for (; !version_iter.end(); ++version_iter) {
+    try {
+      Version v;
+      v.decode_json(*version_iter);
+      result.emplace_back(v);
+    } catch (const JSONDecoder::err& ex) {
+      ldout(m_cct, 2) << __func__
+                      << ": failed to decode update responder JSON. "
+                      << ex.what() << dendl;
+      return std::nullopt;
+    }
+  }
+  return result;
+}
+
+void S3GWTelemetry::do_update() {
+  JSONFormatter formatter;
+  std::ostringstream os;
+  bufferlist response;
+  const auto now = ceph::real_clock::now();
+
+  create_update_responder_request(&formatter);
+  formatter.flush(os);
+
+  m_state.update_attempt(now);
+  const bool success = post_to_update_responder(os.str(), response);
+  ldout(m_cct, 20) << __func__
+                   << ": s3gw telemetry response: " << response.to_str()
+                   << dendl;
+  ldout(m_cct, 20) << __func__ << ": s3gw telemetry request: " << os.str()
+                   << dendl;
+
+  if (success) {
+    auto parsed_response = parse_upgrade_response(response);
+    if (parsed_response.has_value()) {
+      m_state.update_success(now, parsed_response.value());
+    }
+  }
+}
+
+void S3GWTelemetry::append_sfs_telemetry(JSONFormatter* f) const {
+  if (!m_sfs) {
+    return;
+  }
+  encode_json(
+      "sfs_avail_kb",
+      std::to_string(m_sfs->filesystem_stats_avail_bytes / 1024), f
+  );
+  encode_json(
+      "sfs_total_kb",
+      std::to_string(m_sfs->filesystem_stats_total_bytes / 1024), f
+  );
+
+#if defined(__linux__)
+  const auto& data_path = m_sfs->get_data_path();
+  struct statfs stat;
+  int ret = statfs(data_path.c_str(), &stat);
+  if (ret == 0) {
+    switch (stat.f_type) {
+      case EXT4_SUPER_MAGIC:
+        encode_json("sfs_fs", "ext4", f);
+        break;
+      case XFS_SUPER_MAGIC:
+        encode_json("sfs_fs", "xfs", f);
+        break;
+      default:
+        encode_json(
+            "sfs_fs", "unknown magic " + std::to_string(stat.f_type), f
+        );
+        break;
+    }
+  } else {
+    encode_json("sfs_fs", "unknown", f);
+  }
+#else
+  encode_json("sfs_fs", "unknown", f);
+#endif
+}
+
+void S3GWTelemetry::create_update_responder_request(JSONFormatter* f) const {
+  std::map<std::string, std::string> sys_info;
+  collect_sys_info(&sys_info, m_cct);
+
+  uint64_t cgroup_mem_limit;
+  get_cgroup_memory_limit(&cgroup_mem_limit);
+
+  f->open_object_section("version");
+  encode_json("appVersion", ceph_version_to_str(), f);
+
+  f->open_object_section("extraInfo");
+  for (const char* key :
+       {"ceph_version", "ceph_version_short", "ceph_release", "os",
+        "kernel_version", "kernel_description", "arch", "mem_total_kb",
+        "mem_swap_kb", "cpu", "container_image"}) {
+    if (const auto maybe_val = sys_info.find(key);
+        maybe_val != sys_info.end()) {
+      encode_json(key, maybe_val->second, f);
+    }
+  }
+  encode_json("cgroup_mem_limit", std::to_string(cgroup_mem_limit), f);
+  append_sfs_telemetry(f);
+  f->close_section();  // extraInfo
+  f->close_section();  // version
+}

src/rgw/rgw_s3gw_telemetry.h
@@ -1,0 +1,139 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=2 sw=2 expandtab ft=cpp
+
+/*
+ * Ceph - scalable distributed file system
+ * SFS SAL implementation
+ *
+ * Copyright (C) 2023 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ *
+ */
+#ifndef RGW_S3GW_TELEMETRY_H
+#define RGW_S3GW_TELEMETRY_H
+
+#include <boost/beast/http/status.hpp>
+#include <chrono>
+#include <mutex>
+#include <random>
+#include <string>
+
+#include "common/Formatter.h"
+#include "common/ceph_mutex.h"
+#include "common/ceph_time.h"
+#include "rgw_sal_sfs.h"
+#include "rgw_status_page.h"
+
+/// S3GWTelemetry periodically sends s3gw version and system
+/// information to a Longhorn update responder. Makes the the replied
+/// available versions information available.
+class S3GWTelemetry {
+ public:
+  /// Status stores the timestamps of the last update process
+  struct Status {
+    ceph::real_time last_attempt;
+    ceph::real_time last_success;
+  };
+
+  /// Version stores (partial) version information parsed from JSON
+  /// replies. See
+  /// https://github.com/longhorn/upgrade-responder/blob/master/upgraderesponder/service.go
+  /// Version struct
+  struct Version {
+    std::string name;
+    ceph::real_time release_date;
+
+    void decode_json(JSONObj* obj);
+  };
+
+ private:
+  class State {
+   private:
+    ceph::mutex m_mutex;
+    /// parsed version information from the last successful update
+    std::vector<Version> m_versions;
+    /// Keeps update status information
+    Status m_status;
+
+   public:
+    State() : m_mutex(make_mutex("S3GWTelemetry::State")), m_versions(), m_status({{}, {}}) {}
+    Status status() const {
+      std::lock_guard<std::mutex> lock(mutex);
+      return m_status;
+    }
+    std::vector<Version> versions() const {
+      std::lock_guard<std::mutex> lock(mutex);
+      return m_versions;
+    }
+    void update_attempt(const ceph::real_time next_attempt) {
+      std::lock_guard<std::mutex> lock(mutex);
+      m_status.last_attempt = next_attempt;
+    }
+    void update_success(
+        const ceph::real_time next_success,
+        const std::vector<Version>& next_versions
+    ) {
+      std::lock_guard<std::mutex> lock(mutex);
+      m_status.last_success = next_success;
+      m_versions = next_versions;
+    }
+  } m_state;
+
+  CephContext* const m_cct;
+  const rgw::sal::SFStore* const m_sfs;
+
+  bool m_shutdown;
+  std::thread m_updater;
+  ceph::condition_variable m_updater_cvar;
+  ceph::mutex m_updater_mutex;
+
+  void updater_main(std::chrono::milliseconds update_interval);
+  void append_sfs_telemetry(JSONFormatter* f) const;
+
+  /// Make POST to upgrade responder URL. Stores the raw response body data in response
+  bool post_to_update_responder(const std::string& body, bufferlist& response)
+      const;
+
+ protected:
+  /// Send request, parse response, update internal state on success
+  void do_update();
+
+  /// Wake up service thread
+  void wake_up();
+
+ public:
+  S3GWTelemetry(CephContext* cct, const rgw::sal::SFStore* sfs);
+  virtual ~S3GWTelemetry() = default;
+
+  /// Start service thread. After call will peridodically send and
+  /// receive update information.
+  void start();
+
+  /// Stop telemetry service thread.
+  void stop();
+
+  /// Schedule update immediately. Only call after start(), before stop()
+  void update();
+
+  /// Return available versions. Populated after successful update. Otherweise empty.
+  std::vector<Version> available_versions() const;
+
+  /// Return status information regarding sending / receiving update.
+  Status status() const;
+
+  /// Parse "CheckUpgradeResponse"
+  /// (https://github.com/longhorn/upgrade-responder/blob/master/upgraderesponder/service.go)
+  std::optional<std::vector<Version>> parse_upgrade_response(
+      bufferlist& response
+  ) const;
+  
+  /// Create "CheckUpgradeRequest"
+  /// (https://github.com/longhorn/upgrade-responder/blob/master/upgraderesponder/service.go)
+  void create_update_responder_request(JSONFormatter* f) const;
+};
+
+#endif  // RGW_S3GW_TELEMETRY_H

commit d394cc732b83f43a3e8e0e18e2188783c39d15f9

rgw/s3gw: ensure s3gw_telemetry is non-null before stop
env.s3gw_telemetry is only valid when using the sfs store.  If you
try to run the radosgw binary using, say, dbstore, it will segfault
on exit due to a null pointer dereference.

Fixes: 9b02b73a9ea6cafa3d4151951d7c7917a1ba597d
Signed-off-by: Tim Serong <tserong@suse.com>

src/rgw/rgw_appmain.cc
@@ -645,5 +645,7 @@ void rgw::AppMain::shutdown(std::function<void(void)> finalize_async_signals)
 #endif
   rgw_perf_stop(g_ceph_context);
   ratelimiter.reset(); // deletes--ensure this happens before we destruct
-  env.s3gw_telemetry->stop();
+  if (env.s3gw_telemetry) {
+    env.s3gw_telemetry->stop();
+  }
 } /* AppMain::shutdown */